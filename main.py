3a28da4098428ec53b67cce982ffd5d2cd3addf8
Jakub Bednarz <bednaszjakub@gmail.com>
2022-02-04 20:54:12 +0100

    p, q , kanoniczna

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
+from tkinter import font
 
 root = Tk()
 root.resizable(width=None, height=None)
 root.title("delta\u00b2")
 
-v = Canvas(root, width=540, height=400).pack()
+v = Canvas(root, width=540, height=700).pack()
 
 x2 = Entry(v, width=5, font="Helvetica 20 bold")
 x2.place(x=70, y=30)
@@ -21,27 +22,34 @@ Label(v, text=" = 0", font="Helvetica 20 bold", fg='#444444').place(x=420, y=30)
 
 
 def delta():
-    a = int(x2.get())
-    b = int(x1.get())
-    c = int(x0.get())
+    a = float(x2.get())
+    b = float(x1.get())
+    c = float(x0.get())
 
     dt = (b**2) - (4*a*c)
+    p = round(((-1*b)/(2*a)),2)
+    q = round(((-1*dt)/(4*a)),2)
 
-    dt_l = Label(v, text=f"\u0394 = {dt}", font="Helvetica 20 bold").place(x=225, y=190)
-
+    dt_l = Label(v, text=f"\u0394 = {dt}", font="Helvetica 20 bold").place(x=215, y=190)
+    p_l = Label(v, text=f"p = {p}", font="Helvetica 15 bold").place(x=140, y=360)
+    q_l = Label(v, text=f"q = {q}", font="Helvetica 15 bold").place(x=320, y=360)
+    if p>0:
+        p_k = Label(v, text=f"Postać kanoniczna: f(x) = {a}(x-{p})\u00b2+({q})", font="Helvetica 15 bold").place(x=50, y=420)
+    else:
+         p_k = Label(v, text=f"Postać kanoniczna: f(x) = {a}(x+{abs(p)})\u00b2+({q})", font="Helvetica 15 bold").place(x=60, y=420)
     if dt > 0:
         mz1 = round((((-1)*b) + (dt**(1/2))) / (2*a), 2)
         mz2 = round((((-1)*b) - (dt**(1/2))) / (2*a), 2)
         mz1_l = Label(v, text=f"x\u2081 = {mz1}", font="Helvetica 15 bold").place(x=220, y=260)
         mz2_l = Label(v, text=f"x\u2082 = {mz2}", font="Helvetica 15 bold").place(x=220, y=310)
-        return dt_l, mz1_l, mz2_l
+        return dt_l, mz1_l, mz2_l, p_l, q_l, p_k
     elif dt == 0:
         mz1 = ((-1)*b) / (2*a)
         mz1_l = Label(v, text=f"x1 = {mz1}", font="Helvetica 15 bold").place(x=220, y=260)
-        return dt_l, mz1_l
+        return dt_l, mz1_l, p_l, q_l, p_k
     elif dt < 0:
-        mz0_l = Label(v, text="brak miejsc zerowych", font="Helvetica 15 bold").place(x=165, y=260)
-        return dt_l, mz0_l
+        mz0_l = Label(v, text="brak miejsc zerowych", font="Helvetica 15 bold").place(x=165, y=280)
+        return dt_l, mz0_l, p_l, q_l, p_k
 
     
 
